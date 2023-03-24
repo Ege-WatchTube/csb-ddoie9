a4139efe9ef50e4ad8d402bb59289c6ad2ffa2fa
Code Sandbox <hello@codesandbox.io>
2023-03-24 11:59:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d9776c752b4f564e6fb2877fc3d1b707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01dec9a8cd19140336d56d20d8a5131d",
+    "url": "https://.github.io/csb-ddoie9/index.html"
+  },
+  {
+    "revision": "6d1f088bbb82ef0d740d",
+    "url": "https://.github.io/csb-ddoie9/static/css/main.d8bda0e6.chunk.css"
+  },
+  {
+    "revision": "d4377a3ecc8dd2371aac",
+    "url": "https://.github.io/csb-ddoie9/static/js/2.63e322ab.chunk.js"
+  },
+  {
+    "revision": "23394db3b9cb10669f52b14d9b487edd",
+    "url": "https://.github.io/csb-ddoie9/static/js/2.63e322ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1f088bbb82ef0d740d",
+    "url": "https://.github.io/csb-ddoie9/static/js/main.a1ad9fed.chunk.js"
+  },
+  {
+    "revision": "9c79874c0b3a4bde884e",
+    "url": "https://.github.io/csb-ddoie9/static/js/runtime-main.936943ad.js"
+  }
+]);
